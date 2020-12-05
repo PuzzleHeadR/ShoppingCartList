e7540fe8a1aebdd05a283db6c500f50b7fad8268
PuzzleHeadR <66564219+PuzzleHeadR@users.noreply.github.com>
2020-12-05 14:27:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //v3.4 Add popup describing app when visitors load webpage the first time
 window.onload = function() {
-    alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
+    alert("Welcome to 'To-do List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n");
     populateshoppinglistonload();
     displayShoppinglists();
     clearFocus();
@@ -66,8 +66,7 @@ function copyToClipboard(text) {
 
 function about()
 {
-    alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
-    
+    alert("Welcome to 'To-do List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\n
 }
 //read cookie and return
 function readCookie(name) {
