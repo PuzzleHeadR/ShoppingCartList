86cc359ea24ff95e4b807db10b589b4e86321148
PuzzleHeadR <66564219+PuzzleHeadR@users.noreply.github.com>
2020-12-10 08:26:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function get(name){
 //v4.1 ShareList via bitly api
 function passlist()
 {
- var url = "https://rvclist.github.io/index.html?list="+ shoppinglist;
+ var url = "https://To-doListDirectTaskManager.github.io/index.html?list="+ shoppinglist;
     var accessToken = "eff66075988ab2f610fba18455b493ce90540381";
 
     var params = {
